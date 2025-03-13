47f05446b4a94474b9d9a19eb3888bcbd44f75d5
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-13 09:53:39 -0400

    add commit button, refactor clearGrid() to refreshGrid()

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 console.log("Page Loaded")
 const container = document.querySelector(".sketchContainer");
 const resizeButton = document.querySelector("#resizeButton");
+const clearGridButton = document.querySelector("#clearGridButton");
 let gridSize = 16;
 resizeButton.addEventListener("click",() => {
     gridSize = prompt("Grid Size? (Max is 100)");
-    clearGrid();
-    drawGrid(gridSize);
+    refreshGrid();
 })
 
+clearGridButton.addEventListener("click",refreshGrid)
 
-
-function clearGrid() {
+function refreshGrid() {
+    console.log("Grid Clear")
     while(container.firstChild) {
         container.firstChild.remove();
     }
+    drawGrid(gridSize);
 }
 
 function drawGrid(gridSize) {
+    // Limits grid size to 100 to conserve system resources
+    gridSize = Math.min(100,gridSize);
     for (y=0;y<gridSize;y++) {
         const row = document.createElement("div");
         row.classList.add("row");
